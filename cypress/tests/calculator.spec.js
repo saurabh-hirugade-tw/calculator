528a9fa92ca596fac8ccc3dffbf8e2c8393a6425
saurabh.hirugade <saurabh.hirugade@saurabhhirugades-MacBook-Pro.local>
2022-04-25 11:38:06 +0530

    Added logic to make calculator work

--- cypress/tests/calculator.spec.js
+++ cypress/tests/calculator.spec.js
@@ -15,3 +15,32 @@ describe("Rendering tests", () => {
     cy.get(".digits").should("exist");
   });
 });
+
+describe("Operation tests",() => {
+    beforeEach(() => {
+      cy.visit("/");
+      cy.get(".digits").contains("1").click();
+      cy.get(".operators").contains("+").click();
+      cy.get(".digits").contains("2").click();
+    })
+  
+    it("Should output three when one is added with two", ()=>{
+      
+      cy.get(".digits").contains("=").click();
+      cy.get(".output").contains("3").click();
+    });
+  
+    it("Should delete last digit typed", () => {
+      
+      cy.get(".operators").contains("Del").click();
+      cy.get(".operation").invoke('text').then((text) => {
+        expect(text).to.not.have.string("2");
+      });
+    })
+  
+    it("Should clear all output", () =>{
+      cy.get(".operators").contains("Clear").click();
+      cy.get(".result").should("be.empty");
+      cy.get(".operation").should("be.empty");
+    })
+  });
--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import "./styles.css";
+import { useState } from "react";
+import { evaluate } from "mathjs";
+
 function App() {
+  const [operation, setOperation] = useState("");
+  const [result, setResult] = useState("");
+  const ops = ["/", "*", "+", "-", "."];
+
+  const evaluateResult = (expr) => {
+    let newExpr;
+    if (ops.includes(expr.slice(-1))) newExpr = expr.slice(0, -1);
+    else newExpr = expr;
+    return evaluate(newExpr).toString();
+  };
+
+  const updateOperation = (value) => {
+    if (
+      (operation === "" && ops.includes(value)) ||
+      (ops.includes(operation.slice(-1)) && ops.includes(value))
+    )
+      return;
+    setOperation(operation + value);
+    if (!ops.includes(value)) {
+      setResult(evaluateResult(operation + value));
+    }
+  };
+
+  const equate = () => {
+    const answer = evaluateResult(operation);
+    setOperation(answer);
+    setResult("");
+  };
+
+  const deleteLast = () => {
+    if (operation !== "") {
+      const value = operation.slice(0, -1);
+      setOperation(value);
+      setResult(evaluateResult(operation));
+    }
+    return;
+  };
+
+  const clearAll = () => {
+    setOperation("");
+    setResult("");
+  };
 
   const createDigitButtons = () => {
     const digits = [];
     for (let i = 1; i < 10; i++) {
       digits.push(
-        <button key={i}>
+        <button key={i} onClick={() => updateOperation(i.toString())}>
           {i}
         </button>
       );
@@ -16,23 +61,23 @@ function App() {
   return (
     <div className="calculator">
       <div className="output">
-        <div className="result">{}</div>
-        <div className="operation">{}</div>
+        <div className="result">{result}</div>
+        <div className="operation">{operation}</div>
       </div>
       <div className="operators">
-        <button >Clear</button>
-        <button >Del</button>
-        <button>/</button>
-        <button>*</button>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={clearAll}>Clear</button>
+        <button onClick={deleteLast}>Del</button>
+        <button onClick={() => updateOperation("/")}>/</button>
+        <button onClick={() => updateOperation("*")}>*</button>
+        <button onClick={() => updateOperation("+")}>+</button>
+        <button onClick={() => updateOperation("-")}>-</button>
       </div>
       <div className="digits">
-      {createDigitButtons()}
-      <button>0</button>
-      <button>.</button>
-      <button>=</button>
-    </div>
+        {createDigitButtons()}
+        <button onClick={() => updateOperation("0")}>0</button>
+        <button onClick={() => updateOperation(".")}>.</button>
+        <button onClick={equate}>=</button>
+      </div>
     </div>
   );
 }
